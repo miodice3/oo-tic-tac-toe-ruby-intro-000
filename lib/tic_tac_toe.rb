51081387b372e148b9dae7b4d001991fdb04cd92
Michael I <miodice3@gmail.com>
2020-09-25 22:53:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,9 +90,9 @@ class TicTacToe
 =end
 
       def current_player
-        if turn_count(@board) % 2 == 0
+        if turn_count % 2 == 0
           "X"
-        elsif turn_count(@board) % 2 == 1
+        elsif turn_count % 2 == 1
           "O"
         else
           return
