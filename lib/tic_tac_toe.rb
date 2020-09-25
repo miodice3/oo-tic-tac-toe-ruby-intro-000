a9e01b2760a692cd008f0c6ba2da429ed6506d36
Michael I <miodice3@gmail.com>
2020-09-25 22:30:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,7 +48,8 @@ class TicTacToe
     end
 
     def valid_move?(index)
-      index.between?(0,8) && !position_taken?(@board, index)
+      if index.between?(0,8 = position_taken?(@board, index)
+        true
     end
     
     def turn(board)
