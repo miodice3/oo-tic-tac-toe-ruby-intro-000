8c6b0ec0ce3a2ad5d40cc0163b24dd68f09b555a
Michael I <miodice3@gmail.com>
2020-09-25 22:39:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,9 +46,13 @@ class TicTacToe
         nil
       end
     end
-
+=begin
     def valid_move?(index)
       index.between?(0,8) && !position_taken?(@board,index)
+    end
+=end
+    def valid_move?
+      @index.between?(0,8) && !position_taken?(@board,@index)
     end
 
     
