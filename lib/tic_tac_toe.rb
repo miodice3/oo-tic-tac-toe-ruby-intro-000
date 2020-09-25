e37201f9356bfd335d9836d8979315bb059ea2e0
Michael I <miodice3@gmail.com>
2020-09-25 21:04:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,7 +53,23 @@ class TicTacToe
       @index = index
       @index.between?(0,8) && !position_taken?(@board, @index)
     end
+    
+    def turn(board)
+      @board = board
+        puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  #puts index
+  if valid_move?(board, index) == true
+    move(board, index, sign=current_player(board))
+    display_board(board)
+  else
+   puts "your move is not valid, please try again"
+   turn(board)
+ end
 
+      
+    end 
+  
 
- 
 end 
