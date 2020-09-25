5c07f5d46f55fac0c5ae51209d492c8404d7e1bb
Michael I <miodice3@gmail.com>
2020-09-25 21:48:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,8 +19,7 @@ class TicTacToe
       ]
 	  
 	  
-	  def display_board            #(board)
-	    #@board = board
+	  def display_board            
 	      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
         puts "-----------"
         puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
